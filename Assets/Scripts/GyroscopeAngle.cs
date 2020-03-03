b68eaba13dd046ed96d17f7074dfb6c0c76ad9c3
Peter <pjthompson.au@gmail.com>
2020-03-03 12:50:44 +1100

    MarbleGameGyroscope

---
+++ Assets/Scripts/GyroscopeAngle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeAngle : MonoBehaviour
+{	
+	void Start()
+	{
+		transform.rotation = new Quaternion(0, 0, 0, 0);
+
+		if (!Input.gyro.enabled)
+		{
+			Input.gyro.enabled = true;
+		}
+	}
+
+	void Update()
+	{
+		transform.rotation = Input.gyro.attitude;
+	}
+}
